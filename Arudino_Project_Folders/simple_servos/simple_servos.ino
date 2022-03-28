6873d768351a7f59b0239435f19b21fd69427a8c
rhit-bryantlj <bryantlj@rose-hulman.edu>
2022-03-28 10:55:29 -0400

    added bluetooth basic folder

--- Arudino_Project_Folders/simple_servos/simple_servos.ino
+++ Arudino_Project_Folders/simple_servos/simple_servos.ino
@@ -13,27 +13,34 @@ int motorPin = 33;
 
 
 void setup() {
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
   Serial.begin(115200);
-  steeringServo.attach(servoPin); // attaches the servo on pin 18 to the servo object
+  
+//  steeringServo.attach(servoPin); // attaches the servo on pin 18 to the servo object
 
-
-//  motorServo.setPeriodHertz(50);
-//  motorServo.attach(motorPin);
+  motorServo.setPeriodHertz(50);    // standard 50 hz servo
+  motorServo.attach(motorPin, 1000, 2000);
 //  motorServo.write(100);
 }
 
 void loop() {
- 
-//  motorServo.write(80);
+
+ // run motor 2 seconds then turn off
+  motorServo.write(80);
+  delay(2000);
+  motorServo.write(0);
 //  delay(2000);
   
-//  motorServo.write(0);
-  for (pos = 10; pos <= 170; pos += 1) { // goes from 0 degrees to 180 degrees
+    // Sweep servo back and forth
+  for (pos = 120; pos <= 160; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     steeringServo.write(pos);    // tell servo to go to position in variable 'pos'
     delay(15);             // waits 15ms for the servo to reach the position
   }
-  for (pos = 170; pos >= 10; pos -= 1) { // goes from 180 degrees to 0 degrees
+  for (pos = 160; pos >= 20; pos -= 1) { // goes from 180 degrees to 0 degrees
     steeringServo.write(pos);    // tell servo to go to position in variable 'pos'
     delay(15);             // waits 15ms for the servo to reach the position
   }
