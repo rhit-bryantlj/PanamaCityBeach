f21d76ce4c64bdb799a0b7f6c9fb1136e967e5dd
rhit-bryantlj <bryantlj@rose-hulman.edu>
2022-03-30 10:02:08 -0400

    Milestone 1 software complete

--- Arudino_Project_Folders/simple_servos/simple_servos.ino
+++ Arudino_Project_Folders/simple_servos/simple_servos.ino
@@ -1,16 +1,22 @@
-
 #include <ESP32Servo.h>
+#include <Wire.h>
+#include <Adafruit_INA219.h>
 
 Servo steeringServo;  // create servo object to control a servo
 Servo motorServo;
 
+Adafruit_INA219 ina219; // component for measuring power
+
 int pos = 0;    // variable to store the servo position
 int motorSpeed = 0; // var to control motorSpeed
+float shuntvoltage = 0;
+float busvoltage = 0;
+float current_mA = 0;
+float loadvoltage = 0;
+float power_mW = 0;
 
 int servoPin = 32;
 int motorPin = 33;
-
-
 
 void setup() {
   ESP32PWM::allocateTimer(0);
@@ -18,30 +24,60 @@ void setup() {
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
   Serial.begin(115200);
+  while(!Serial) {
+    // will pause Zero, Leonardo, etc until serial console opens
+    delay(1);
+  }
+
+  // Initialize the INA219.
+  // By default the initialization will use the largest range (32V, 2A).  However
+  // you can call a setCalibration function to change this range (see comments).
+  if (! ina219.begin()) {
+    Serial.println("Failed to find INA219 chip");
+    while (1) { delay(10); }
+  }
+  
   
 //  steeringServo.attach(servoPin); // attaches the servo on pin 18 to the servo object
 
   motorServo.setPeriodHertz(50);    // standard 50 hz servo
   motorServo.attach(motorPin, 1000, 2000);
-//  motorServo.write(100);
+  delay(500);
+  motorServo.write(120);
 }
 
 void loop() {
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+  
+  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
+  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+  Serial.println("");
+
+  delay(2000);
+
+
 
  // run motor 2 seconds then turn off
-  motorServo.write(80);
-  delay(2000);
-  motorServo.write(0);
+//  motorServo.write(80);
+//  delay(2000);
+//  motorServo.write(0);
 //  delay(2000);
   
     // Sweep servo back and forth
-  for (pos = 120; pos <= 160; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    steeringServo.write(pos);    // tell servo to go to position in variable 'pos'
-    delay(15);             // waits 15ms for the servo to reach the position
-  }
-  for (pos = 160; pos >= 20; pos -= 1) { // goes from 180 degrees to 0 degrees
-    steeringServo.write(pos);    // tell servo to go to position in variable 'pos'
-    delay(15);             // waits 15ms for the servo to reach the position
-  }
+//  for (pos = 120; pos <= 160; pos += 1) { // goes from 0 degrees to 180 degrees
+//    // in steps of 1 degree
+//    steeringServo.write(pos);    // tell servo to go to position in variable 'pos'
+//    delay(15);             // waits 15ms for the servo to reach the position
+//  }
+//  for (pos = 160; pos >= 20; pos -= 1) { // goes from 180 degrees to 0 degrees
+//    steeringServo.write(pos);    // tell servo to go to position in variable 'pos'
+//    delay(15);             // waits 15ms for the servo to reach the position
+//  }
 }
