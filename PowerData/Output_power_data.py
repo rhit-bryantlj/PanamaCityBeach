80cff3cd528daae13ce09ed962a94b688c5be9bc
rhit-bryantlj <bryantlj@rose-hulman.edu>
2022-05-15 17:19:30 -0400

    power data additions made

--- PowerData/Output_power_data.py
+++ PowerData/Output_power_data.py
@@ -1,15 +1,29 @@
 import re
+from xml.etree.ElementTree import tostring
 
 
-outputFile = open('fig-8-pwr-68-65-9.5-laps.csv','w')
+outputFile = open('test_out.csv','w')
 
-with open('fig-8-pwr-68-65-9.5-laps.txt', 'r', errors='replace') as f:
+outputFile.write("Time (s),shunt V (mV),bus V ,current mA,power mW,load V \n")
+timeNum = 0.5
+
+with open('fig-8-pwr-67-67-10.8-laps.txt', 'r', errors='replace') as f:
     lines = f.readlines()
     for line in lines:
-        # print(line[0])
+        # print(line)
         result = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-        for float in result:
-            outputFile.write(float)
+        timeins = str(timeNum)
+        outputFile.write(timeins)
+        outputFile.write(",")
+        for floatString in result:
+            outputFile.write(floatString)
             outputFile.write(",")
+        timeNum += 0.5
         outputFile.write("\n")
         # print(result)
+    outputFile.write("\n")
+    outputFile.write(" ,Average, Max \n")
+    outputFile.write("shunt V (mV),=AVERAGE(B2:B239),=MAX(B2:B239) \n")
+    outputFile.write("current mA ,=AVERAGE(D2:D239),=MAX(D2:D239) \n")
+    outputFile.write("power mW ,=AVERAGE(E2:E239),=MAX(E2:E239), ,Joules,=SUM(E2:E239)/(2*1000)\n")
+    
